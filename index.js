daa0abea7684838efd93dfdc7d694a93720f14e8
amosmaundundu <amosmaundu5@gmail.com>
2022-08-18 08:45:47 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+    logShout(hello);
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper(hello);
+
+function sayHiToHeadphonedRoommate(string){
+    if (string===string.toLowerCase());{
+        return "hello , I can't hear you";
+
+
+    }
+    function sayHiToHeadphonedRoommate(string){
+    if (string===string.toUpperCase());{
+        return "YES INDEED"}
+
+    function sayHiToHeadphonedRoommate(string)
+    if("let's have dinner"()){
+        return "I would love to";}
+    
+    
+   
+    
+}
+
+    }
